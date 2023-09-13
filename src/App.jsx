d97b55f2a40cc11405e65d0d5be5685d44ef71dd
ibrohim5009 <ibrohimabdurayimov5@gmail.com>
2023-09-13 12:52:23 +0300

    otash_mol

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./components/home/Home";
+
+const App = () => {
+  return (
+    <div className="bg-[#F7F5FA] container mx-auto">
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navabar from "./navbar/Navabar";
+import HeroHeader from "./hero-header/HeroHeader";
+
+function Home() {
+  return (
+    <div className="">
+      <Navabar />
+      <HeroHeader />
+    </div>
+  );
+}
+
+export default Home;
