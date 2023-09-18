ed821aec06e5ad37994587ac1c98a7f81a3443d8
xakimovotabek200 <xakimovoatbek@gmail.com>
2023-09-18 14:22:32 +0500

    add footerHEader

--- src/components/Footer/FooterHEader.jsx
+++ src/components/Footer/FooterHEader.jsx
@@ -10,11 +10,24 @@ function FooterHEader() {
   return (
     <div className='w-[95%] container mx-auto mt-[5pc]'>
       <div className='w-[95%] h-[420px] bg-[#0A033C] rounded-3xl md:flex justify-between'>
-        <div>
-          <img src={Ellipse1} alt="" />
+        <div className='p-[50px]'>
+          <img className='object-cover' src={Ellipse1} alt="" />
+          <img className='object-cover pl-[120px] pt-[50px]' src={Ellipse2} alt="" />
+          <img className='object-cover pt-[100px] ' src={Ellipse3} alt="" />
         </div>
-        <div>middle</div>
-        <div>right</div>
+        <div>
+          <h1 className='text-white'>
+            Subscribe For Get Update <br /> Every New Courses
+          </h1>
+          <span className='text-[#FFFFFFB2]'>20k+ students daily learn with Eduvi. Subscribe for new courses.</span>
+        </div>
+        <div>
+          <div className='p-[50px]'>
+          <img className='object-cover' src={Ellipse4} alt="" />
+          <img className='object-cover pl-[120px] pt-[50px]' src={Ellipse5} alt="" />
+          <img className='object-cover pt-[100px] ' src={Ellipse6} alt="" />
+        </div> 
+        </div>
       </div>
     </div>
   )
