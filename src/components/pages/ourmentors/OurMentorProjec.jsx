bafd401d9b4ee403389db864a0f91571505434ae
xakimovotabek200 <xakimovoatbek@gmail.com>
2023-09-20 14:19:37 +0500

    remove option

--- src/components/pages/ourmentors/OurMentorProjec.jsx
+++ src/components/pages/ourmentors/OurMentorProjec.jsx
@@ -11,14 +11,7 @@ function OurMentorsProject({ item }) {
             <h3 className="text-2xl font-semibold capitalize mb-3">{item.name}</h3>
             <div className="flex items-center justify-around">
                 <h4 className="w-[260px] items-center text-[#5D5A6F] text-base font-normal leading-8 mb-3">{item.lastName}</h4>
-                <select name="" id="">
-                    <option value="">
-                        ibrohimjon
-                    </option>
-                    <option value="">
-                        <img src={item.des} alt="" />
-                    </option>
-                </select>
+              
             </div>
         </div>
     );
